0ba5e7ad2c1421ee6da4204b3b40ff495f8f6a6b
Kabir <kbdude.16@gmail.com>
2024-09-10 14:14:57 +0530

    updating database

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
